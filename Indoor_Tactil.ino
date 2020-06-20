a4fa7bc6cebe1062c7c9fa314245692de4e665d3
relyq <nicoale_mainenti@hotmail.es>
2020-06-20 08:56:32 -0300

    updated ver num

--- Indoor_Tactil.ino
+++ Indoor_Tactil.ino
@@ -1,13 +1,4 @@
 /*
-  INDOOR TACTIL v0.75
-
-  sistema de pantallas completamente actualizado
-  la funcion tsMenu() se encarga del manejo del sistema de pantallas
-
-  procedimiento para agregar pantallas:
-    crear el diseño de la pantalla en "s_Screens.ino"
-    crear el inicializador de pantalla en "m_Menus.ino"
-    crear los botones de la pantalla en "_defines.ino"
-    agregar funcionalidad a los botones en "t_tsMenu.ino"
+  GROWOS v0.8
 
 */
