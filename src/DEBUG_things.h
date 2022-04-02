a5086d6d546e8bb1a8eb14d1bec85b00fd2142fa
relyq <nicoale_mainenti@hotmail.es>
2022-04-02 16:58:02 -0300

    src folder name change

---
+++ src/DEBUG_things.h
@@ -0,0 +1,24 @@
+#ifndef _DEBUG_THINGS_H_
+#define _DEBUG_THINGS_H_
+
+#define DEBUG_ENABLED 1
+
+#ifdef DEBUG_ENABLED
+#define DEBUG_BEGIN(x) Serial.begin(x)
+#define DEBUG_PRINT(x) Serial.print(x)
+#define DEBUG_PRINTLN(x) Serial.println(x)
+#define DEBUG_PRINTDEC(x) Serial.print(x, DEC)
+#define DEBUG_PRINTHEX(x) Serial.print(x, HEX)
+#define DEBUG_PRINTLNDEC(x) Serial.println(x, DEC)
+#define DEBUG_PRINTLNHEX(x) Serial.println(x, HEX)
+#else
+#define DEBUG_BEGIN(x)
+#define DEBUG_PRINT(x)
+#define DEBUG_PRINTLN(x)
+#define DEBUG_PRINTDEC(x)
+#define DEBUG_PRINTHEX(x)
+#define DEBUG_PRINTLNDEC(x)
+#define DEBUG_PRINTLNHEX(x)
+#endif
+
+#endif  // _DEBUG_THINGS_H_
